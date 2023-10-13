8450fbae330ba591d24e227f1b28aa40ef8f4d01
tolaoni11 <tolaoni11@yahoo.com>
2023-10-12 23:33:43 -0500

    Header added

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Container.scss";
 
-export default function Container() {
-  return <div className="container">Container</div>;
+export default function Container({ children }) {
+  return <div className="container">{children}</div>;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Container from "../components/container/Container";
+import Header from "../components/header/Header";
 
 export default function Home() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 }
