75cc94f6743473e8302a6b5f572e57d66b6a9d88
tolaoni11 <tolaoni11@yahoo.com>
2023-10-12 23:11:07 -0500

    Container added

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Container.scss";
+
+export default function Container() {
+  return <div className="container">Container</div>;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Container from "../components/container/Container";
 
 export default function Home() {
-  return <div>Home</div>;
+  return <Container></Container>;
 }
