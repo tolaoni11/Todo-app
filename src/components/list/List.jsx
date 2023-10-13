ccead77339926ee6bff84533226e000a4dbc695f
tolaoni11 <tolaoni11@yahoo.com>
2023-10-13 00:47:39 -0500

    List Items cont. added

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -8,6 +8,21 @@ export default function List() {
         <div className="list__item">Buy New laptop</div>
         <div className="list__delete">Del</div>
       </div>
+
+      <div className="list__row">
+        <div className="list__item">Study Codehance Courses</div>
+        <div className="list__delete">Del</div>
+      </div>
+
+      <div className="list__row">
+        <div className="list__item">Make a site</div>
+        <div className="list__delete">Del</div>
+      </div>
+
+      <div className="list__row">
+        <div className="list__item">Revise Assignment</div>
+        <div className="list__delete">Del</div>
+      </div>
     </div>
   );
 }
